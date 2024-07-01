43c65d4a7a9e89346313a628aa102ee2284f5716
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2024-06-30 21:28:03 -0700

    annotated the server file

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // requires express
+const app = express() // calls express and stores it in a variable 'app'
+const MongoClient = require('mongodb').MongoClient //gives access to mongodb
+const PORT = 2121 // variable for port in case it needs to be changed
+require('dotenv').config() //required for .env to store sensitive data
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db, // declares db
+    dbConnectionStr = process.env.DB_STRING, // declares connection string. stores variable in env string
+    dbName = 'todo' // declares the database name in mongo
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // awaits mongo. connects mongodb
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+        console.log(`Connected to ${dbName} Database`) 
+        db = client.db(dbName) // connects to db within mongo
     })
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs') // sets up view engine to ejs
+app.use(express.static('public')) // able to use public folder for static files
+app.use(express.urlencoded({ extended: true })) // able to parse incoming request bodies that are url encoded
+app.use(express.json()) // able to use json requests with express
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ // get request. root directory
+    const todoItems = await db.collection('todos').find().toArray() //awaits the todo collection within the db
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // awaits another collection. marks it as not completed
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // render the index ejs file. names of the items is todoItems and the items left are itemsLeft. 
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,59 +35,59 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+app.post('/addTodo', (request, response) => { // post request. api endpoint  is addTodo
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // takes name from form and assigns it to thing. it inserts that one thing into the collection on mongo
     .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+        console.log('Todo Added') // console notification
+        response.redirect('/') // redirects to root
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // throws error
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markComplete', (request, response) => { // put (update) request. endpoint is markComplete
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // targets one and updates the thing
         $set: {
-            completed: true
+            completed: true // sets to true
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // sorts by id descending
+        upsert: false // upsert inserts rows into db table. 
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete') // lets developer know its done
+        response.json('Marked Complete') // lets user know its done
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))// throws error
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+app.put('/markUnComplete', (request, response) => { // put request. marks incomplete
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // targets item and updates thing
         $set: {
-            completed: false
+            completed: false // sets to false
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // sorts by id descending
+        upsert: false // upsert inserts rows into db table. 
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete') // lets developer know its don
+        response.json('Marked Complete') // lets user know its done
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // throws error
 
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+app.delete('/deleteItem', (request, response) => { // deletes request. endpoint is deleteItem
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // targets thing and deletes the one thing
+    .then(result => { 
+        console.log('Todo Deleted')// lets developer know its done
+        response.json('Todo Deleted') // lets user know its done
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))// throws error
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, ()=>{ // listens for
+    console.log(`Server running on port ${PORT}`) // consoles port
 })
